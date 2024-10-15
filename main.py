2d59a1276239e6511dbcb7ee0fc9c1555a5ffa39
aleksandar-stotka <aangelevski6@gmail.com>
2024-10-15 20:20:31 +0200

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''
+"""
 import random
 
 print("Welcome to Rock, Paper, Scissors!\n")
@@ -71,31 +71,86 @@ while True:
         print("Thanks for playing!")
         break
 
-'''
+"""
+
 ##############
 
 # password generator
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
-for char in range(0,nr_letters):
-     password_list.append(random.choice(letters))    
-     
-for char in range(0,nr_symbols):
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_symbols):
     password_list.append(random.choice(symbols))
-    
-for char in range(0,nr_numbers):
+
+for char in range(0, nr_numbers):
     password_list.append(random.choice(numbers))
-    
+
 random.shuffle(password_list)
 print(password_list)
 password = ""
@@ -109,21 +164,21 @@ print(f"ypur password is:  {password}")
 
 
 
-    
+def test_drive():
+    a = 4
+    if a < 3:
+        print("ok")
+    else:
+        a > 52
+        print("not")   
+        
+        
+test_drive()
 
 
-
-   
-
-                                
-    
-    
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-    
-    
-    
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
